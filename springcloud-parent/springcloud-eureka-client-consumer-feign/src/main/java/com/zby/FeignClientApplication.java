8759ae56699a2011948df900f823c9e7dee45ea3
zhoubaiyun <zhoubaiyun@lixin360.com>
2018-05-07 11:39:05 +0800

    feign踩坑

--- springcloud-parent/springcloud-eureka-client-consumer-feign/src/main/java/com/zby/FeignClientApplication.java
+++ springcloud-parent/springcloud-eureka-client-consumer-feign/src/main/java/com/zby/FeignClientApplication.java
@@ -5,18 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
-/**
- * 
- * @author zby
- * @description 这是一个大坑，使用这个类名会报错，留在这儿警醒世人吧
- *
- */
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-public class FeignClient {
+public class FeignClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FeignClient.class, args);
+		SpringApplication.run(FeignClientApplication.class, args);
 	}
 }
